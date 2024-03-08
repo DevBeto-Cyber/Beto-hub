32f9acdf391badae862e717ce8fabef4b7b0a5a5
Roberto <147996450+Roberto353656@users.noreply.github.com>
2024-03-08 17:22:01 -0300

    Update BetoHub.lua

--- BetoHub.lua
+++ BetoHub.lua
@@ -1,4 +1,3 @@
--------------------------------------------------------
 local ScreenGui1 = Instance.new("ScreenGui")
 local ImageButton1 = Instance.new("ImageButton")
 local UICorner = Instance.new("UICorner")
@@ -539,4 +538,4 @@ local LogoToggle = Instance.new("ImageLabel")
                     ):Play()
                 else
                     toggled = false
-                    Circle:TweenPosition(UDim2.new(0,2,0,2),"Out","Sine",0.2
+                    Circle:TweenPosition(UDim2.new(0,2,0,2),"Out","Sine",0.2,tru
