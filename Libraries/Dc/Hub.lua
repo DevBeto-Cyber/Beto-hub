710ed35841aaf1efd76439790b90a92a24fc2311
Dev Beto <147996450+JosejamesDev@users.noreply.github.com>
2024-05-20 17:52:42 -0300

    commit

--- Libraries/Dc/Hub.lua
+++ Libraries/Dc/Hub.lua
@@ -709,7 +709,7 @@ A:AddButton("idle",function()
 ---------------------------------------------------                 --//Esp\\--
 
 E:AddButton("Esp Box",function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Roberto353656/Beto-hub/main/EspBox.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JosejamesDev/Beto-hub/main/Others/EspBox.lua"))()
 end)
 
 E:AddButton("Red Circle",function()
@@ -796,11 +796,11 @@ end)
 --------------------------------------------------
                   --//Others\\--
   O:AddButton("Anti-Fling",function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Roberto353656/Beto-hub/main/Anti-fling.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JosejamesDev/Beto-hub/main/Others/Anti-fling.lua"))()
   end)
   
   O:AddButton("Shiftlock",function()
-          loadstring(game:HttpGet('https://raw.githubusercontent.com/Unknownproootest/Permanent-Shift-Lock-Beta/alt/PermShiftlockV2-alt'))()
+          loadstring(game:HttpGet('https://raw.githubusercontent.com/JosejamesDev/Beto-hub/main/Others/Shiftlock.lua'))()
       end
       )
       
@@ -810,12 +810,16 @@ end)
     )
     
   O:AddButton("Chat spy",function()
-           loadstring(game:HttpGet("https://raw.githubusercontent.com/Roberto353656/Beto-hub/main/Chat_spy.lua"))()
+           loadstring(game:HttpGet("https://raw.githubusercontent.com/JosejamesDev/Beto-hub/main/Others/Spy.lua"))()
     end
     )
-  O:AddButton("Translator", function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Roberto353656/Beto-hub/main/Translator.lua"))() end
+  O:AddButton("Translator", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JosejamesDev/Beto-hub/main/Others/Translator.lua"))() end
     )
-  
+  O:AddButton("HttpSpy",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JosejamesDev/Beto-hub/main/Others/HttpSpy.lua"))()
+    end
+    )
   
 ---------------------------------------------------
 print("Script Carregado com sucesso")
